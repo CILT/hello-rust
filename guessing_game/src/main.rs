09c474495e44ff3ab2345fd424b30441d50e00cc
Cristián Llull <cllullt@gmail.com>
2024-05-15 23:44:55 -0400

    Finish guessing game tutorial

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,25 +7,39 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is {secret_number}");
+    // Only debug
+    // println!("The secret number is {secret_number}");
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    // stdin() expects an expect, but it can compile
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line!");
+        // read_line puts the user input into any string we pass to
+        // it, but also returns an Enumeration value (Result).
+        // Result can be Ok or Err.
+        // Result has expect() method, which handles both cases.
+        // If not handled, compiler throws:
+        //   warning: unused `Result` that must be used
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
 
-    // Trim eliminates any whitespace at beginning or end
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        // Trim eliminates any whitespace at beginning or end
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
     }
 }
